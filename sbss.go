f423ae4f1bc84e4e7c73613666af1128fee2d5ca
supar <paulrez@gmail.com>
2016-11-22 17:54:45 +0300

    Init

---
+++ sbss.go
@@ -0,0 +1,206 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"crypto/md5"
+	"crypto/sha1"
+	"encoding/hex"
+	"encoding/json"
+	"errors"
+	"github.com/google/go-querystring/query"
+	"io"
+	"net/http"
+	"net/url"
+	"strconv"
+)
+
+type AuthResponse struct {
+	Success   bool   `json:"success"`
+	Authorize bool   `json:"authorized"`
+	Login     string `json:"login"`
+	Challenge int64  `json:"challenge"`
+	Cname     string `json:"cname"`
+}
+
+type Client struct {
+	*http.Client
+	authorized bool
+	UserAgent  string
+}
+
+type AuthRequest struct {
+	Async     uint   `url:"async"`
+	Authorize string `url:"authorize"`
+	Login     string `url:"login"`
+	Password  string `url:"-"`
+	Remember  uint   `url:"remember"`
+}
+
+// Create new http client
+func NewClient() (c *Client, err error) {
+	c = &Client{
+		Client:    &http.Client{},
+		UserAgent: "Sbss-Client",
+	}
+
+	return
+}
+
+// Authenticate manager
+func (this *Client) Login(urlStr string, auth *AuthRequest) (err error) {
+	var (
+		res    *http.Response
+		result *AuthResponse
+	)
+
+	if this.authorized {
+		return
+	}
+
+	if res, err = this.authRequest(urlStr, auth); err != nil {
+		return err
+	}
+
+	result = &AuthResponse{}
+	if err = ReadResponse(res, &result); err != nil {
+		return err
+	}
+
+	// Connection is authorized
+	if result.Success {
+		this.authorized = true
+		return nil
+	}
+
+	// Otherwise send authentication
+	// Create login with challenge
+	auth.Authorize = authKey(auth, result.Challenge)
+	if res, err = this.authRequest(urlStr, auth); err != nil {
+		return err
+	}
+	auth.Authorize = auth.Login
+
+	result = &AuthResponse{}
+	if err = ReadResponse(res, &result); err != nil {
+		return err
+	}
+
+	if !result.Success {
+		err = errors.New("Not authorized")
+	}
+
+	this.authorized = true
+	return
+}
+
+// Check if auithorized
+func (this *Client) authRequest(urlStr string, auth *AuthRequest) (res *http.Response, err error) {
+	var (
+		data *bytes.Buffer
+		req  *http.Request
+	)
+
+	if auth == nil {
+		return nil, errors.New("Authentication data required (login/password)")
+	}
+
+	if data, err = EncodeForm(auth); err != nil {
+		return nil, err
+	}
+
+	// Prepare request just with login field
+	// to check if connection is authorized
+	// otherwise server will return challenge to authenticate
+	if req, err = this.NewRequest("POST", urlStr, data); err != nil {
+		return nil, err
+	}
+
+	// Send request
+	if res, err = this.Do(req); err != nil {
+		return nil, err
+	}
+
+	return
+}
+
+// Override NewRequest to add User_agent header
+func (this *Client) NewRequest(method, urlStr string, body io.Reader) (r *http.Request, err error) {
+	if r, err = http.NewRequest(method, urlStr, body); err != nil {
+		return
+	}
+
+	r.Header.Set("User-Agent", this.UserAgent)
+	r.Header.Set("X-Requested-With", "XMLHttpRequest")
+
+	if method == "POST" {
+		r.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	}
+
+	return
+}
+
+func NewAuthRequest(login, password string) *AuthRequest {
+	return &AuthRequest{
+		Async:     1,
+		Authorize: login,
+		Login:     login,
+		Password:  password,
+		Remember:  0,
+	}
+}
+
+// Encode struct to the form-data
+func EncodeForm(data interface{}) (buf *bytes.Buffer, err error) {
+	var (
+		v url.Values
+	)
+
+	if v, err = query.Values(data); err != nil {
+		return nil, err
+	}
+
+	return bytes.NewBufferString(v.Encode()), nil
+}
+
+// Read response body to remove every leading symbol
+// unless fist right tocken will be found
+//
+// Decode JSON response
+func ReadResponse(r *http.Response, target interface{}) (err error) {
+	var (
+		buf    = bufio.NewReader(r.Body)
+		tokens = []byte("{}[]")
+	)
+
+	defer r.Body.Close()
+
+	// remove leading character unless right tocken will be found
+	for {
+		if r, _ := buf.Peek(1); !bytes.Contains(tokens, r) {
+			buf.Read(r)
+
+			continue
+		}
+
+		break
+	}
+
+	return json.NewDecoder(buf).Decode(&target)
+}
+
+// Create authentication signature
+func authKey(auth *AuthRequest, ch int64) string {
+	var (
+		m5 = md5.New()
+		s1 = sha1.New()
+	)
+
+	io.WriteString(m5, auth.Login)
+	io.WriteString(m5, auth.Password)
+
+	io.WriteString(s1, hex.EncodeToString(m5.Sum(nil)))
+	io.WriteString(s1, strconv.FormatInt(ch, 10))
+
+	return hex.EncodeToString(s1.Sum(nil))
+}
---
+++ sbss_test.go
@@ -0,0 +1,246 @@
+package main
+
+import (
+	"bytes"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+const UserAgent = "Sbss-Client"
+
+func NewTestServer(handler http.Handler) (ts *httptest.Server) {
+	ts = httptest.NewServer(handler)
+	return
+}
+
+func Test_LoginAuthStruct(t *testing.T) {
+	var (
+		a = []struct {
+			login, password string
+			result          []byte
+		}{
+			{
+				login:    "user1",
+				password: "password1",
+				result:   []byte("async=1&authorize=user1&login=user1&remember=0"),
+			},
+		}
+
+		b *AuthRequest
+	)
+
+	for i, v := range a {
+		b = NewAuthRequest(v.login, v.password)
+
+		if r, err := EncodeForm(b); err != nil {
+			t.Errorf("Unexpected error %s", err.Error())
+		} else {
+			if !bytes.Equal(r.Bytes(), v.result) {
+				t.Errorf("Unexpected result at index %d", i)
+			}
+		}
+	}
+}
+
+func Test_CheckUserAgentHeader(t *testing.T) {
+	var (
+		cli, _ = NewClient()
+
+		err error
+		r   *http.Request
+	)
+
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		u := UserAgent
+		if v := r.UserAgent(); v != u {
+			t.Errorf("Expected User-Agent %s, but got %s", u, v)
+		}
+	})
+
+	ts := NewTestServer(handler)
+	defer ts.Close()
+
+	if r, err = cli.NewRequest("GET", ts.URL, nil); err != nil {
+		t.Error(err)
+	}
+
+	if _, err = cli.Do(r); err != nil {
+		t.Error(err)
+	}
+}
+
+func Test_ReadResponseWithInvalidLeadingCharacters(t *testing.T) {
+	var (
+		cli, _ = NewClient()
+		a      = []struct {
+			in, out []byte
+		}{
+			{
+				in: []byte(`({"success":true})`),
+			},
+			{
+				in: []byte(` ( {"success":true} )`),
+			},
+			{
+				in: []byte(`\t\n\t ( {
+					"success":true}`),
+			},
+			{
+				in: []byte(`({"success":false,"authorize":false,"login":null,"challenge":8545724,"cname":"3b47a663b0765fe1"})`),
+			},
+		}
+
+		err error
+		r   *http.Request
+		res *http.Response
+	)
+
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		data, _ := ioutil.ReadAll(r.Body)
+		w.Write(data)
+	})
+
+	ts := NewTestServer(handler)
+	defer ts.Close()
+
+	for i, v := range a {
+		if r, err = cli.NewRequest("POST", ts.URL, bytes.NewReader(v.in)); err != nil {
+			t.Error(err)
+			continue
+		}
+
+		if res, err = cli.Do(r); err != nil {
+			t.Error(err)
+			continue
+		}
+
+		target := AuthResponse{}
+		if err = ReadResponse(res, &target); err != nil {
+			t.Error(err)
+			continue
+		}
+
+		if (i < 3 && !target.Success) || (i > 2 && target.Success) {
+			t.Errorf("Unexpected value at index ", i)
+		}
+	}
+}
+
+func Test_AuthKeyValid(t *testing.T) {
+	var (
+		a = &AuthRequest{
+			Login:    "user1",
+			Password: "password1",
+		}
+		c int64 = 7374616523
+		r       = "5c00a9b7b6cf5b0dc4f79ecac1976bcbe8dbe1e9"
+	)
+
+	if res := authKey(a, c); res != r {
+		t.Errorf("Unexpected result %s, expected %s", res, r)
+	}
+}
+
+func Test_AuthenticationStatusSuccess(t *testing.T) {
+	var (
+		cli, _ = NewClient()
+		lauth  = NewAuthRequest("user1", "")
+		r      = &AuthResponse{}
+
+		err error
+		res *http.Response
+	)
+
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.PostFormValue("login") == lauth.Login {
+			io.WriteString(w, `({"success":true})`)
+
+			return
+		}
+
+		io.WriteString(w, `({"success":false,"authorize":false,"login":null,"challenge":5698316,"cname":"3b47a663b0765fe1"})`)
+	})
+
+	ts := NewTestServer(handler)
+	defer ts.Close()
+
+	if res, err = cli.authRequest(ts.URL, lauth); err != nil {
+		t.Error(err)
+	}
+
+	if err = ReadResponse(res, r); err != nil {
+		t.Error(err)
+	}
+
+	if !r.Success {
+		t.Error("Expected success:true")
+	}
+}
+
+func Test_AuthenticationStatusFail(t *testing.T) {
+	var (
+		cli, _ = NewClient()
+		lauth  = NewAuthRequest("user1", "password1")
+		r      = &AuthResponse{}
+
+		err error
+		res *http.Response
+	)
+
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, `({"success":false,"authorize":false,"login":null,"challenge":5698316,"cname":"3b47a663b0765fe1"})`)
+	})
+
+	ts := NewTestServer(handler)
+	defer ts.Close()
+
+	if res, err = cli.authRequest(ts.URL, lauth); err != nil {
+		t.Error(err)
+	}
+
+	if err = ReadResponse(res, r); err != nil {
+		t.Error(err)
+	}
+
+	if r.Success {
+		t.Error("Expected success:false")
+	}
+
+	if r.Challenge == 0 {
+		t.Error("Expected not empty challenge")
+	}
+}
+
+func Test_LoginSuccess(t *testing.T) {
+	var (
+		cli, _ = NewClient()
+		lauth  = NewAuthRequest("user1", "password1")
+		c      = "85c0f07a48e711b8c91923f3b6779e737f7e39cc"
+
+		err error
+	)
+
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.PostFormValue("login") == lauth.Login && r.PostFormValue("authorize") == c {
+			io.WriteString(w, `({"success":true})`)
+
+			return
+		}
+
+		io.WriteString(w, `({"success":false,"authorize":false,"login":null,"challenge":5698316,"cname":"3b47a663b0765fe1"})`)
+	})
+
+	ts := NewTestServer(handler)
+	defer ts.Close()
+
+	if err = cli.Login(ts.URL, lauth); err != nil {
+		t.Error(err)
+	}
+
+	if !cli.authorized {
+		t.Error("Expected authorized:true in the struct")
+	}
+}
